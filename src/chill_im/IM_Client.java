aba0ecd5131fbb207561f5f95fb7f374e3d8dc7a
Colin Hill <colin@peak2peakmedia.com>
2016-09-17 12:08:22 +0200

    [Add] IM_Client and IM_Server files, with outlines of what to do [Add] .gitignore file

---
+++ src/chill_im/IM_Client.java
@@ -0,0 +1,21 @@
+package chill_im;
+
+/**
+ * Created by chill on 9/17/16.
+ */
+public class IM_Client {
+    // Global Vars
+
+    // Contains Methods to handle Protocols
+
+    // userAdd(String data)
+    // userRemove(String data)
+    // sendDisconnect()
+    // writeUsers()
+
+    // Thread --> Incoming Reader
+        // Run () --> Handles Protocol Commands
+            // Contains Switch to call methods accordingly
+
+
+}
---
+++ src/chill_im/IM_Server.java
@@ -0,0 +1,23 @@
+package chill_im;
+
+/**
+ * Created by chill on 9/17/16.
+ */
+public class IM_Server {
+    // Client Handler
+        // Create new Clients and save their references in Array Lists
+        // Run()
+            // Switch to route the protocols to the correct place
+
+
+
+    // Server Thread
+        // Run()
+            // "Create the server"
+        // Create all Protocol Methods
+            // User Add
+            // User Remove
+            // List
+            // Tell Everyone
+            // ALVE
+}
