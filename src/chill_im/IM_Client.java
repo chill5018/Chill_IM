87d8757e3d35b0fd38af5af77fb4fb55f8a8e5fe
Colin Hill <colin@peak2peakmedia.com>
2016-09-17 13:10:53 +0200

    [Add] Created Server socket [Add] Created Client Connection to Server

--- src/chill_im/IM_Client.java
+++ src/chill_im/IM_Client.java
@@ -1,10 +1,144 @@
 package chill_im;
 
+import com.sun.corba.se.pept.transport.ListenerThread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by chill on 9/17/16.
  */
 public class IM_Client {
     // Global Vars
+    static String userName, address;
+    AbstractList<String> users = new ArrayList<>();
+    private static final int PORT = 1234;
+    private static Boolean isConnected = false;
+
+    // Network Globals
+    static Socket host;
+    private static BufferedReader reader;
+    private static PrintWriter networkOutput;
+    private static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        do {
+            setUserName();
+            configMenu();
+            if (JOIN(userName, host)) {
+//                ALVE();
+//                DATA();
+                System.out.println("Success");
+            }
+        } while (!JOIN(userName, host));
+
+    }
+
+    private static void configMenu(){
+        String hostIP;
+        System.out.println(
+                "Enter 1: for Manual Config " +
+                        "\nEnter 2: for LocalHost Config\n");
+        switch (userInput.nextInt()) {
+            case 1:
+
+                System.out.println("Enter Server IP: ");
+                hostIP = userInput.nextLine();
+                hostIP += userInput.nextLine();
+
+                System.out.print("Enter Server PORT: ");
+                int port = userInput.nextInt();
+
+                try {
+                    host = new Socket(hostIP, port);
+                } catch (IOException e) {
+                    System.out.println("\nHost ID not found!");
+                    System.exit(1);
+                }
+                break;
+            case 2:
+                hostIP = "127.0.0.1";
+                try {
+                    host = new Socket(hostIP, PORT);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    private static void setUserName() {
+        userInput = new Scanner(System.in);
+
+        System.out.print("Enter UserName: ");
+        userName = userInput.next();
+
+        if (userName.length() > 12) {
+            System.out.println("Max 12 Characters!");
+            System.out.println("Please Try Again...");
+            System.out.print("Enter UserName: ");
+            userName = userInput.next();
+        }
+    }
+
+    private static boolean JOIN(String userName, Socket host){
+        // Containers for messages, and responses
+        String response ="";
+        try {
+            // Send message to server  via network output stream...
+            // Receive responses from server via network input stream
+
+            InputStreamReader streamReader = new InputStreamReader(host.getInputStream());
+            reader = new BufferedReader(streamReader);
+            networkOutput = new PrintWriter(host.getOutputStream());
+
+
+
+            // JOIN Protocol client --> server
+            networkOutput.println("JOIN "+userName+" , "+host.getInetAddress().getHostAddress()+" : "+
+                    host.getPort()+"");
+
+            networkOutput.flush();
+            isConnected = true;
+
+
+            //  Authentication Response from Server
+//            response = networkInput.nextLine();
+
+            //Display Server's response to user..
+//            System.out.println("\nSERVER> " + response);
+
+        } catch (IOException ioEx){
+            ioEx.printStackTrace();
+        }
+
+        ListenThread();
+
+        // Authentication Message from Server
+        if (response.equals("J_OK")){
+            // Allow Access
+            System.out.println("Access Granted");
+        } else if (response.equals("J_ERR")) {
+            // Try a Diff User Name
+            System.out.println("Access Denied");
+            return false;
+        }
+        return true;
+    }
+
+    public static void ListenThread()
+    {
+        Thread IncomingReader = new Thread(new IncomingReader());
+        IncomingReader.start();
+    }
 
     // Contains Methods to handle Protocols
 
@@ -14,8 +148,35 @@ public class IM_Client {
     // writeUsers()
 
     // Thread --> Incoming Reader
+
+    public static class IncomingReader implements Runnable {
+
         // Run () --> Handles Protocol Commands
+        @Override
+        public void run() {
+            String rawData;
+
             // Contains Switch to call methods accordingly
+            try {
+                //Accept Messages from Client on the Socket's Input stream...
+                rawData = reader.readLine();
+
+                // Create a token Scanner to reac the first TOKEN == Keyword
+                Scanner token = new Scanner(rawData);
+
+                String cmd = token.next();
+                System.out.println("CMD: " + cmd);
+
+                // Log What the Users sends to Server
+                System.out.println(host.getLocalAddress().getHostAddress() + ": " + rawData + "\n");
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+
+
+        }
+    }
+
 
 
 }
--- src/chill_im/IM_Server.java
+++ src/chill_im/IM_Server.java
@@ -1,19 +1,96 @@
 package chill_im;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * Created by chill on 9/17/16.
  */
 public class IM_Server {
-    // Client Handler
-        // Create new Clients and save their references in Array Lists
-        // Run()
+    //Globals
+    private static ArrayList clientOutputStreams;
+    ArrayList<String> users;
+
+    public static void main(String[] args) {
+        IMServer server = new IMServer();
+        server.run();
+    }
+
+    // TODO: Move this to a separate File
+    // Client Handler START
+    public static class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket clientSocket;
+        PrintWriter clientWriter;
+
+        public ClientHandler(Socket clientSocket, PrintWriter clientWriter) {
+            this.clientSocket = clientSocket;
+            this.clientWriter = clientWriter;
+
+            try {
+                InputStreamReader isReader = new InputStreamReader(clientSocket.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (IOException io){
+                System.out.println("Unexpected Error ");
+            }
+        }
+
+        @Override
+        public void run() {
             // Switch to route the protocols to the correct place
+
+        }
+    } // End --> ClientHandler
 
 
 
     // Server Thread
-        // Run()
+    public static class IMServer implements Runnable {
+        @Override
+        public  void run() {
+
+            clientOutputStreams = new ArrayList();
             // "Create the server"
+
+            try {
+                ServerSocket serverSocket = new ServerSocket(1234);
+
+                System.out.println(serverSocket.getInetAddress().getHostAddress() +" "+ serverSocket.getLocalPort());
+
+
+                do {
+                    // Accept incoming client connections
+                    Socket clientSocket = serverSocket.accept();
+
+                    // Get the clients outputWriter (meaning messages client sends to server)
+                    PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream());
+
+                    // Add client Writer to the list of client output streams
+                    clientOutputStreams.add(clientWriter);
+
+                    // Create new Client Handler Thread to listen to Clients
+                    Thread listener = new Thread(new ClientHandler(clientSocket, clientWriter));
+                    listener.start();
+
+                    System.out.println("Connection Made...");
+
+
+                } while (true);
+
+
+            } catch (IOException io) {
+                System.out.println("Error Making Connection...");
+            }
+
+        }
+    }
+
+
         // Create all Protocol Methods
             // User Add
             // User Remove
